974aa1b557f69c42a2913c0ebec76a17a52765eb
dezsim <dezsimartin@gmail.com>
2021-09-10 12:44:59 +0200

    createChannel endpoint implemented

--- src/api/router.ts
+++ src/api/router.ts
@@ -73,6 +73,15 @@ const createRouter = () => {
     controller.channels
   );
 
+  router.post(
+    "/channels/create",
+    validators.bodyDiscordId("serverId"),
+    validators.bodyDiscordId("roleId"),
+    validators.channelNameValidator,
+    validators.categoryNameValidator,
+    controller.createChannel
+  );
+
   router.get(
     "/administeredServers/:userHash",
     validators.paramUserHash("userHash"),
