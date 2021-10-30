77cf1a5cd09e0fa97af87595cab8d6fdbb9157b2
DevidAbt <abt.devid@gmail.com>
2021-10-30 16:16:07 +0200

    Added simple command version of join slash command

--- src/discords/SimpleCommands.ts
+++ src/discords/SimpleCommands.ts
@@ -6,14 +6,14 @@ import {
   SimpleCommandMessage,
   SimpleCommandOption,
 } from "discordx";
-import { ping, status } from "../commands";
+import { join, ping, status } from "../commands";
 import Main from "../Main";
 import logger from "../utils/logger";
 import { getUserDiscordId, getUserHash } from "../utils/utils";
 
 @Discord()
 abstract class SimpleCommands {
-  static commands = ["ping", "status"];
+  static commands = ["ping", "status", "join"];
 
   @SimpleCommand("ping")
   ping(command: SimpleCommandMessage): void {
@@ -42,7 +42,9 @@ abstract class SimpleCommands {
     }
 
     logger.verbose(
-      `!status command was used by ${command.message.author.username}#${
+      `${command.prefix}status command was used by ${
+        command.message.author.username
+      }#${
         command.message.author.discriminator
       } -  targeted: ${!!userHashParam} userHash: ${userHash} userId: ${
         user.id
@@ -57,6 +59,27 @@ abstract class SimpleCommands {
 
     const embed = await status(user, userHash);
     command.message.reply({ embeds: [embed] }).catch(logger.error);
+  }
+
+  @SimpleCommand("join")
+  async join(command: SimpleCommandMessage) {
+    if (command.message.channel.type === "DM") {
+      command.message.reply(
+        "Use this command in a server to join all of its guilds you have access to!"
+      );
+      return;
+    }
+
+    logger.verbose(
+      `${command.prefix}join command was used by ${command.message.author.username}#${command.message.author.discriminator}`
+    );
+
+    const message = await join(
+      command.message.author.id,
+      command.message.guild.id
+    );
+
+    command.message.reply(message);
   }
 }
 
